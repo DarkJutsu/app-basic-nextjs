538be1570027f58e2f62a8cf9daf75cb947fba97
DarkJutsu <ulloajose34@gmail.com>
2026-01-11 01:45:02 -0600

    feat(pages):implement static props to fetch and display sorted blog posts

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,37 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
+import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section>
-        <p>Soy salvadoreño con una amplia experiencia en el desarrollo de Software. Especializado en Java, NextJS, Kotlin, JetPack Compose.</p>
+        <p>Historia que tú hicistes, Historia por hacer.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        <section className={`${utilStyles.headingMg} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem}>{title}<br />{id}<br />{date}</li>
+            ))}
+          </ul>
+        </section>
       </section>
     </Layout>
   )
