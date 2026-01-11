0a954f752ec309450054bda0b00db31811385388
DarkJutsu <ulloajose34@gmail.com>
2026-01-11 16:32:36 -0600

    feat:add Date component for formatting post dates

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,8 @@
+import Head from "next/head";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+import utilStyle from '../../styles/utils.module.css'
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id)
@@ -17,8 +20,18 @@ export async function getStaticPaths() {
   }
 }
 export default function Post({ postData }) {
-  return <Layout>
-    {postData.title}<br />{postData.id}<br />{postData.date}
-    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-  </Layout>
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyle.headingXl}>{postData.title}</h1>
+        <div className={utilStyle.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
 }
